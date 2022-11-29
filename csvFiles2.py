890f5ac702f222e8f9931576e082584fd0c5f3cb
JSLee311 <98490645+JSLee311@users.noreply.github.com>
2022-11-28 21:59:07 -0500

    Add files via upload

---
+++ csvFiles2.py
@@ -0,0 +1,36 @@
+import csv
+import pandas as pd
+import os
+import glob
+
+
+def readCSV():
+# Get only csv files.
+    for csvFilename in os.listdir('.'): 
+        if not csvFilename.endswith('.csv'):
+            continue # skip non-csv files 
+        
+    with open(csvFilename, 'r') as file:
+        csvReader = csv.reader(file)
+        for row in csvReader:
+            print(row)
+
+readCSV()
+
+# Using glob
+#path = os.getcwd()
+#csv_files = glob.glob(os.path.join(path, "*.csv"))
+    
+# loop over the list of csv files
+#for f in csv_files:
+      
+    # read the csv file
+#    df = pd.read_csv(f)
+      
+    # print the location and filename
+#    print('Location:', f)
+#    print('File Name:', f.split("\\")[-1])
+      
+    # print the content
+#    print('Content:')
+#    print(df)
